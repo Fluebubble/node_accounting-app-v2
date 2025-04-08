0a96ae6d9627aed45fd2526b88742e7daaa57995
Fluebubble <Trots.anatoly@gmail.com>
2025-04-08 13:34:26 +0200

    testing

---
+++ src/services/users.service.js
@@ -0,0 +1,53 @@
+let users = [
+  {
+    id: 0,
+    name: 'Harry Eblov',
+  },
+  {
+    id: 1,
+    name: 'Marry Popkins',
+  },
+  {
+    id: 2,
+    name: 'Svinomatka',
+  },
+];
+
+const getUsers = () => {
+  return users;
+};
+
+const createUser = (name) => {
+  const newUser = {
+    id: Date.now() + Math.floor(Math.random() * 1000),
+    name,
+  };
+
+  users.push(newUser);
+
+  return newUser;
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id) || null;
+};
+
+const deleteUser = (id) => {
+  users = users.filter((user) => user.id !== id);
+};
+
+const updateUser = (id, newName) => {
+  const currentUser = getUser(id);
+
+  currentUser.name = newName;
+
+  return currentUser;
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+};
